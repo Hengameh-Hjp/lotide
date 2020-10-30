d13ab6684fb8e9bd7e5b8987f660a30b1056c9e2
Hana <67531901+hajhana@users.noreply.github.com>
2020-10-29 22:54:11 -0400

    Add files via upload

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const findKey = function(object, callback) {
+  for(let val in object) {
+    if(callback(object[val])) {
+      return val;
+    }
+  }
+}
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ssertion failed:' ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma')
+
