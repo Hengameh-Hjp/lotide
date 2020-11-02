be8a4bc736eabcca36cc3adfbde618fc7727df70
Hana <67531901+hajhana@users.noreply.github.com>
2020-11-01 21:02:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+let a = 'head';
+let b = 'tail';
+let c = 'middle';
+
+module.exports = {
+  a,
+  b,
+  c
+};
+
+
+console.log(module.exports);
